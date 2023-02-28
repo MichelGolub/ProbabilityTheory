0691947f9dedcfb40848e48ed5f6662eb2d0dab1
Mikhail Golub <116822157+MichelGolub@users.noreply.github.com>
2023-02-28 13:42:24 +0100

    Add files via upload

---
+++ ProbabilityTheory.Backend/PrTh.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+using FluentValidation;
+using PrTh.Application.Common.Behaviors;
+
+namespace PrTh.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication
+            (this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services
+                .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
+            return services;
+        }
+    }
+}
