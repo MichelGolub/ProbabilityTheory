890c65b741e36510a8b5aeea49f18273b0d03705
Mikhail Golub <116822157+MichelGolub@users.noreply.github.com>
2023-02-28 13:44:47 +0100

    Add files via upload

---
+++ ProbabilityTheory.Backend/PrTh.Application/Common/Exceptions/FieldConflictException.cs
@@ -0,0 +1,8 @@
+﻿namespace PrTh.Application.Common.Exceptions
+{
+    public class FieldConflictException : Exception
+    {
+        public FieldConflictException(string name, string field, object key)
+            : base($"Entity \"{name}\" with value ({key}) of {field} already exist.") { }
+    }
+}
